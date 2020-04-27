68ad5d8b79ebc3f43faf29d6d196078ac392fdeb
Jan Zborowski <janzborowski99@gmail.com>
2020-04-27 17:13:05 +0200

    Update Book model

--- books/models.py
+++ books/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
+
 class Book(models.Model):
     title = models.CharField(max_length=100)
+    author = models.CharField(max_length=60, null=True)
     short_description = models.TextField()
     published_at = models.DateTimeField()
+
+    def __str__(self):
+        return self.title + (" by " + self.author if self.author else "")
