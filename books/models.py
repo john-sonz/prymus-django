ed87518b0babf55d91230000d33604e806feed39
Jan Zborowski <janzborowski99@gmail.com>
2020-04-27 16:21:16 +0200

    Add book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    short_description = models.TextField()
+    published_at = models.DateTimeField()
