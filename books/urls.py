0869bf827546b1f802aecd48404097687ff02e83
Jan Zborowski <janzborowski99@gmail.com>
2020-05-11 17:09:23 +0200

    Visual changes

--- books/urls.py
+++ books/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 app_name = 'books'
+
 urlpatterns = [
     path('', views.book_list, name='list'),
     path('<int:book_id>/', views.book_detail, name='detail')
