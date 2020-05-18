7de487b1414915a887359427735b5ca5db69799c
Jan Zborowski <janzborowski99@gmail.com>
2020-05-18 12:41:05 +0200

    Update settings file

--- books/views.py
+++ books/views.py
@@ -4,15 +4,19 @@ from django.shortcuts import render
 
 from books.models import Book
 
+
 def index(request):
     return render(request, "index.html")
+
 
 def form(request):
     return render(request, "form.html")
 
+
 def user_profile(request):
     print(request.user)
     return render(request, "profile.html")
+
 
 def user_signup(request):
     if request.method == 'POST':
@@ -20,27 +24,25 @@ def user_signup(request):
         if form.is_valid():
             form.save()
             return render(request, "registration/signup_complete.html")
-        
-        pass
     else:
         form = UserCreationForm()
 
-    return render(request, "registration/signup_form.html", context={'form': form})
+    return render(request,
+                  "registration/signup_form.html",
+                  context={'form': form})
+
 
 def book_list(request):
     books = Book.objects.all()
 
-    context = {
-        "books": books
-    }
+    context = {"books": books}
 
     return render(request, "books.html", context)
+
 
 def book_detail(request, book_id):
     book = Book.objects.get(id=book_id)
 
-    context = {
-        "book": book
-    }
+    context = {"book": book}
 
     return render(request, "book_detail.html", context)
