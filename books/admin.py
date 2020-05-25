aa6326b2b9a37fb0aac08298b5722f71252f5527
Jan Zborowski <janzborowski99@gmail.com>
2020-05-25 11:52:24 +0200

    Migrate and register review model

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
-from books.models import Book, Author
+from books.models import Book, Author, Review
 
 admin.site.register(Book)
 admin.site.register(Author)
-
+admin.site.register(Review)
